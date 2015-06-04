a02cf1ab5790fcf6e6f6ee0310e1845c81e6a53b
dugite-code <dugite-code@users.noreply.github.com>
2015-06-04 15:57:31 +0800

    Re-Import/modification of iframe plugin
    https://github.com/milosz/kolab-octo-iframe

--- config.inc.php
+++ config.inc.php
@@ -1,5 +1,3 @@
 <?php
-// Tiny Tiny RSS URL
-$rcmail_config['ttrss_url'] = '/ttrss';
-// authentication exchange secret
-$rcmail_config['ttrss_secret'] = '<shared-secret-string>';
+// Tiny Tiny RSS location
+$rcmail_config['ttrss_location'] = 'ttrss/;
--- rcmail_ttrss.php
+++ rcmail_ttrss.php
@@ -1,45 +1,38 @@
 <?php
 /**
- * Kolab Tiny Tiny RSS Plugin
+ * Kolab IFRAME Plugin
  *
  * @author Aleksander 'A.L.E.C' Machniak <machniak@kolabsys.com>
- * @author Thomas Bruederli <bruederli@kolabsys.com>
  * @licence GNU AGPL
  *
  * Configuration (see config.inc.php)
  * 
- * 
  * Modified OwnCloud Plugin
- * 
+ *
  * For description visit:
- * http://blog.sleeplessbeastie.eu/2013/06/28/kolab-how-to-integrate-tiny-tiny-rss/
+ * http://blog.sleeplessbeastie.eu/2013/06/24/kolab-how-to-integrate-piwik/
  */
 class rcmail_ttrss extends rcube_plugin
 {
-    // all task excluding 'login'
-   public $task = '?(?!login).*';
+    // all task excluding 'login' and 'logout'
+    public $task = '?(?!login|logout).*';
+    // we've got no ajax handlers
+    public $noajax = true;
     // skip frames
     public $noframe = true;
     function init()
     {
-        // requires kolab_auth plugin
-        if (empty($_SESSION['kolab_uid'])) {
-            // temporary:
-            $_SESSION['kolab_uid'] = $_SESSION['username'];
-            // return;
-        }
         $rcmail = rcube::get_instance();
+
         $this->add_texts('localization/', false);
+		
         // register task
         $this->register_task('rcmail_ttrss');
+		
         // register actions
         $this->register_action('index', array($this, 'action'));
-        $this->add_hook('session_destroy', array($this, 'logout'));
+        $this->register_action('redirect', array($this, 'redirect'));
 		
-        // handler for sso requests sent by the rcmail_ttrss kolab_auth app
-        if ($rcmail->action == 'ttrsssso' && !empty($_POST['token'])) {
-            $this->add_hook('startup', array($this, 'sso_request'));
-        }
         // add taskbar button
         $this->add_button(array(
             'command'    => 'rcmail_ttrss',
@@ -48,13 +41,14 @@ class rcmail_ttrss extends rcube_plugin
             'innerclass' => 'button-inner',
             'label'      => 'rcmail_ttrss.rcmail_ttrss',
             ), 'taskbar');
+			
         // add style for taskbar button (must be here) and Help UI
         $this->include_stylesheet($this->local_skin_path()."/rcmail_ttrss.css");
     }
     function action()
     {
         $rcmail = rcube::get_instance();
-        $rcmail->output->add_handlers(array('ttrssframe' => array($this, 'frame')));
+        $rcmail->output->add_handlers(array('rcmail_ttrssframe' => array($this, 'frame')));
         $rcmail->output->set_pagetitle($this->gettext('rcmail_ttrss'));
         $rcmail->output->send('rcmail_ttrss.rcmail_ttrss');
     }
@@ -62,45 +56,8 @@ class rcmail_ttrss extends rcube_plugin
     {
         $rcmail = rcube::get_instance();
         $this->load_config();
-        // generate SSO auth token
-        if (empty($_SESSION['ttrssauth']))
-            $_SESSION['ttrssauth'] = md5('ttrsssso' . $_SESSION['user_id'] . microtime() . $rcmail->config->get('des_key'));
-        $src  = $rcmail->config->get('ttrss_url');
-        $src .= '?kolab_auth=' . strrev(rtrim(base64_encode(http_build_query(array(
-            'session' => session_id(),
-            'cname'   => session_name(),
-            'token'   => $_SESSION['ttrssauth'],
-        ))), '='));
-        return html::tag('iframe', array('id' => 'ttrssframe', 'src' => $src,
-            'width' => "100%", 'height' => "100%", 'frameborder' => 0));
-    }
-    function logout()
-    {
-        $rcmail = rcube::get_instance();
-        $this->load_config();
-        // send logout request to rcmail_ttrss
-        $logout_url = $rcmail->config->get('ttrss_url') . '/backend.php?op=logout';
-        $rcmail->output->add_script("new Image().src = '$logout_url';", 'foot');
-    }
-    function sso_request()
-    {
-        $response = array();
-        $sign_valid = false;
-        $rcmail = rcube::get_instance();
-        $this->load_config();
-        // check signature
-        if ($hmac = $_POST['hmac']) {
-            unset($_POST['hmac']);
-            $postdata = http_build_query($_POST, '', '&');
-            $sign_valid = ($hmac == hash_hmac('sha256', $postdata, $rcmail->config->get('ttrss_secret', '<undefined-secret>')));
-        }
-        // if rcmail_ttrss sent a valid auth request, return plain username and password
-        if ($sign_valid && !empty($_POST['token']) && $_POST['token'] == $_SESSION['ttrssauth']) {
-            $user = $_SESSION['kolab_uid']; // requires kolab_auth plugin
-            $pass = $rcmail->decrypt($_SESSION['password']);
-            $response = array('user' => $user, 'pass' => $pass);
-        }
-        echo json_encode($response);
-        exit;
+        $src  = $this->urlbase . $rcmail->config->get('ttrss_location') . 'index.php';
+        return '<iframe id="rcmail_ttrssframe" width="100%" height="100%" frameborder="0"'
+            .' src="' . $src. '"></iframe>';
     }
 }
